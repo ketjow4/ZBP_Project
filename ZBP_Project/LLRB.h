03da77b3a4210d14a6276dfbbcdc69fb73376f6a
ketjow4 <wojtek.adam.dudzik@gmail.com>
2016-11-14 18:46:28 +0100

    Added insertion method and ostream operator for debug. Changed everything to ptr

--- ZBP_Project/LLRB.h
+++ ZBP_Project/LLRB.h
@@ -1,6 +1,10 @@
 #ifndef LLRB_H
 #define LLRB_H
 
+#include <iostream>
+#include <iomanip>
+
+using namespace std;
 
 template<typename T>
 struct node
@@ -10,9 +14,17 @@ struct node
 
 	node*   Left;
 	node*   Right;
+	node*	Parent;
 
-	node(T data) : data(data)
-	{}
+	node(T data) : data(data), IsRed(true)
+	{
+		Left = Right = nullptr;
+	}
+
+	node() : IsRed(true)
+	{
+		//data = nullptr;
+	}
 };
 
 template<typename T>
@@ -22,43 +34,43 @@ public:
 	LLRB();
 	~LLRB();
 
+
 	template<typename T>
 	void insert(T data)
 	{
-
+		root = insert(root, data);
+		root->IsRed = false;
 	}
 
 
-	
-
 	template<typename T>
-	node<T> rotateLeft(node<T> h)
+	node<T>* rotateLeft(node<T>* h)
 	{
-		auto x = h.Right;
-		h.Right = x.left;
-		x.left = h;
-		x.IsRed = h.IsRed;
-		h.IsRed = true;
+		auto x = h->Right;
+		h->Right = x->Left;
+		x->Left = h;
+		x->IsRed = h->IsRed;
+		h->IsRed = true;
 		return x;
 	}
 
 	template<typename T>
-	node<T> rotateRight(node<T> h)
+	node<T>* rotateRight(node<T>* h)
 	{
-		auto x = h.Left;
-		h.Left = x.Right;
-		x.Right = h;
-		x.IsRed = h.IsRed;
-		h.IsRed = true;
+		auto x = h->Left;
+		h->Left = x->Right;
+		x->Right = h;
+		x->IsRed = h->IsRed;
+		h->IsRed = true;
 		return x;
 	}
 
 	template<typename T>
-	void flipColors(node<T> h)
+	void flipColors(node<T>* h)
 	{
-		h.IsRed = !h.IsRed;
-		h.Left.IsRed = !h.Left.IsRed;
-		h.Right.IsRed = !h.Right.IsRed;
+		h->IsRed = !h->IsRed;
+		h->Left->IsRed = !h->Left->IsRed;
+		h->Right->IsRed = !h->Right->IsRed;
 	}
 
 	template<typename T>
@@ -85,15 +97,56 @@ public:
 		return nullptr;
 	}
 
+
+	friend ostream& operator<<(ostream& out, LLRB& tree)
+	{
+		postorder(out, tree.root, 0);
+		return out;
+	}
+
+	template<typename T>
+	friend void postorder(ostream& out, node<T>* p, int indent = 0)
+	{
+		if (p != nullptr) {
+			if (p->Right) {
+				postorder(out, p->Right, indent + 4);
+			}
+			if (indent) {
+				out << std::setw(indent) << ' ';
+			}
+			if (p->Right) out << " /\n" << std::setw(indent) << ' ';
+			out << p->data << "\n ";
+			if (p->Left) {
+				out << std::setw(indent) << ' ' << " \\\n";
+				postorder(out, p->Left, indent + 4);
+			}
+		}
+	}
+
 private:
-	node<T> root;
+	node<T>* root;
 
 
 
 	template<typename T>
-	void insert(node<T> h, T data)
+	node<T>* insert(node<T>* h, T data)
 	{
+		if (h == nullptr)
+			return new node<T>(data);
 
+		if (data > h->data)
+			h->Right = insert(h->Right, data);
+		else
+			h->Left = insert(h->Left, data);
+
+		if (IsRed(h->Right) && !IsRed(h->Left))
+			h = rotateLeft(h);
+		if (IsRed(h->Left) && IsRed(h->Left->Left))
+			h = rotateRight(h);
+		if (IsRed(h->Left) && IsRed(h->Right))
+			flipColors(h);
+
+		return h;
 	}
 
 	template<typename T>
@@ -103,27 +156,49 @@ private:
 	}
 
 	template<typename T>
-	node<T> moveRedLeft(node<T> h)
+	node<T>* moveRedLeft(node<T>* h)
 	{
-
+		flipColors(h);
+		if (IsRed(h->Right->Left))
+		{
+			h->Right = rotateRight(h->right);
+			h = rotateLeft(h);
+			flipColors(h);
+		}
+		return h;
 	}
 
 	template<typename T>
-	node<T> moveRedRight(node<T> h)
+	node<T>* moveRedRight(node<T>* h)
 	{
+		flipColors(h);
+		if (IsRed(h->Left->Left))
+		{
+			h = rotateRight(h);
+			flipColors(h);
+		}
+		return h;
+	}
 
+	template<typename T>
+	bool IsRed(node<T>* h)
+	{
+		if (h == nullptr)
+			return false;
+		else
+			return h->IsRed;
 	}
 };
 
-template<typename T>
-LLRB::LLRB()
-{
-}
-
-template<typename T>
-LLRB::~LLRB()
-{
-}
-
 #endif // !LLRB_H
 
+template<typename T>
+inline LLRB<T>::LLRB()
+{
+	root = nullptr;
+}
+
+template<typename T>
+inline LLRB<T>::~LLRB()
+{
+}
--- ZBP_Project/main.cpp
+++ ZBP_Project/main.cpp
@@ -1,10 +1,24 @@
 
-
+#include "LLRB.h"
 
 
 
 int main()
 {
+	LLRB<int> tree;
+
+	tree.insert(1);
+	tree.insert(2);
+	tree.insert(3);
+	tree.insert(4);
+	tree.insert(5);
+	tree.insert(6);
+	tree.insert(7);
+	tree.insert(8);
+	tree.insert(9);
+
+	std::cout << tree;
+
 
 	return 0;
 }
